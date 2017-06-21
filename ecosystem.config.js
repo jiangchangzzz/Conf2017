6f5c7475cc5a2d32083ee8d19f2ea1f27f738786
vienwu <vienwu@tencent.com>
2017-06-21 14:01:22 +0800

    feat:添加部署脚本

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  deploy: {
+    imweb: {
+      user: "root",
+      host: "imweb.io",
+      ref: "origin/master",
+      repo: "git@git.coding.net:jaychen88/imwebconf_2017.git",
+      path: "/data/www/2017.imweb.io",
+      "post-deploy": "cnpm install && bower install && gulp",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  }
+}
