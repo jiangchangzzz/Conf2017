2a46d04396a37a3f25e793236b1d28824978884b
jayccchen <jayccchen@tencent.com>
2017-06-21 15:32:36 +0800

    视差加透明效果

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,81 +1,86 @@
 'use strict';
 
-$(document).ready(function(){
-    //全屏滚动效果
-    
-    if($(window).width()>768){
-        $('#fullpage').fullpage({
-            anchors: ['home','lectuer','sponsor'/*,'information'*/,'about'],
-            navigation: true,
-            navigationPosition: 'right',
-            loopBottom: true,
-            afterLoad: function(anchorLink,index){
-                activeLink($('#header-nav').children().eq(index-1));
-            },
-            scrollOverflow: true
-        });
-    }
-    else{
-        $('#fullpage').fullpage({
-            anchors: ['home','lectuer','sponsor','information','about'],
-            navigation: true,
-            navigationPosition: 'right',
-            loopBottom: true,
-            responsiveWidth: 768,
-            afterLoad: function(anchorLink,index){
-                activeLink($('#header-nav').children().eq(index-1));
-            }
-        });
-    }
+$(document).ready(function() {
+  //全屏滚动效果
 
-    //小屏幕下显示隐藏菜单
-    $('#header-menu').click(function(){
-        $('#header-nav').toggleClass('show');
+  if ($(window).width() > 768) {
+    $('#fullpage').fullpage({
+      anchors: ['home', 'lectuer', 'sponsor' /*,'information'*/ , 'about'],
+      navigation: true,
+      navigationPosition: 'right',
+      loopBottom: true,
+      afterLoad: function(anchorLink, index) {
+        activeLink($('#header-nav').children().eq(index - 1));
+      },
+      scrollOverflow: true
     });
-
-    //点击导航显示选中效果
-    $('#header-nav').delegate('a','click',function(){
-        activeLink($(this));
+  } else {
+    $('#fullpage').fullpage({
+      anchors: ['home', 'lectuer', 'sponsor', 'information', 'about'],
+      navigation: true,
+      navigationPosition: 'right',
+      loopBottom: true,
+      responsiveWidth: 768,
+      afterLoad: function(anchorLink, index) {
+        activeLink($('#header-nav').children().eq(index - 1));
+      }
     });
+  }
 
-    /*var scene=$('#scene').get(0);
-    var parallax=new Parallax(scene);*/
+  //小屏幕下显示隐藏菜单
+  $('#header-menu').click(function() {
+    $('#header-nav').toggleClass('show');
+  });
 
-    //激活导航栏链接
-    function activeLink(ele){
-        ele.siblings('a').removeClass('active');
-        ele.addClass('active');
-    }
+  //点击导航显示选中效果
+  $('#header-nav').delegate('a', 'click', function() {
+    activeLink($(this));
+  });
 
-    //点击切换tab
-    $('#place-list').delegate('li','click',function(){
-        $(this).siblings('li').removeClass('active');
-        $(this).addClass('active');
+  /*var scene=$('#scene').get(0);
+  var parallax=new Parallax(scene);*/
 
-        var target='#'+$(this).data('target');
-        $(target).siblings('.place-content').removeClass('active');
-        $(target).addClass('active');
-    });
+  //激活导航栏链接
+  function activeLink(ele) {
+    ele.siblings('a').removeClass('active');
+    ele.addClass('active');
+  }
 
-    //添加loading页面
-    var imgList=$('.section');
-    var imgNum=imgList.length;
-    for(let i=0,len=imgList.length;i<len;i++){
-        let url=imgList.eq(i).data('url');
-        
-        let img=new Image();
-        img.onload=function(){
-            imgList.eq(i).css('background-image','url('+url+')');
-            imgNum--;
-            if(imgNum===0){
-                $('#loading').hide();
-            }
-        };
-        img.src=url;
-    }
+  //点击切换tab
+  $('#place-list').delegate('li', 'click', function() {
+    $(this).siblings('li').removeClass('active');
+    $(this).addClass('active');
 
-    //设置一个超时，如果时间内没有加载完图片，也进入页面
-    setTimeout(function(){
+    var target = '#' + $(this).data('target');
+    $(target).siblings('.place-content').removeClass('active');
+    $(target).addClass('active');
+  });
+
+  //添加loading页面
+  var imgList = $('.section');
+  var imgNum = imgList.length;
+  for (let i = 0, len = imgList.length; i < len; i++) {
+    let url = imgList.eq(i).data('url');
+
+    let img = new Image();
+    img.onload = function() {
+      imgList.eq(i).css('background-image', 'url(' + url + ')');
+      imgNum--;
+      if (imgNum === 0) {
         $('#loading').hide();
-    },5000);
+        var search = location.search;
+        if (search.indexOf('shicha=1') !== -1) {
+          var scene = document.getElementById('scene');
+          scene.style.display = 'block';
+          var parallax = new Parallax(scene);
+        }
+      }
+    };
+    img.src = url;
+  }
+
+  //设置一个超时，如果时间内没有加载完图片，也进入页面
+  setTimeout(function() {
+    $('#loading').hide();
+  }, 5000);
 });
