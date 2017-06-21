70ad671ab9b9e37dbc4285c9703bfbf3fbef3367
vienwu <vienwu@tencent.com>
2017-06-21 14:16:31 +0800

    up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       host: "imweb.io",
       ref: "origin/master",
       repo: "git@git.coding.net:jaychen88/imwebconf_2017.git",
-      path: "/data/www/2017.imweb.io",
+      path: "/data/www/2017.imweb.io.tmp",
       "post-deploy": "cnpm install && bower install && gulp",
       env: {
         NODE_ENV: "production"
